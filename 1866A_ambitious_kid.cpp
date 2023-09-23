bd26206e44b1096066818c6394ad5bec1093dbfb
Alwan Wirawan <alwanwrwn@student.uns.ac.id>
2023-09-23 22:40:53 +0700

    first commit

---
+++ 1866A_ambitious_kid.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int n, dist, curr=100000; cin>>n;
+	
+	for (int i = 0; i<n; i++) {
+		int num; cin>>num;
+		dist = abs(0-num);
+		curr = min(dist,curr);
+		if (curr==0) {
+			break;
+		}
+	}
+
+	cout<<curr;
+
+	return 0;
+}
